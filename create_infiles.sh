184ed2fb066c690cb9bce1379236669344bb5c5e
nikossrp <41998058+nikossrp@users.noreply.github.com>
2021-05-15 00:24:06 +0300

    Update create_infiles.sh

--- create_infiles.sh
+++ create_infiles.sh
@@ -2,7 +2,7 @@
 
 # execute: ./create_infiles.sh inputFile input_dir numFilesPerDirectory
 
-# You can use inputFile from project1 to create directories for every country
+# Using  inputFile from project1 to create directories for every country
 
 Input_parameters=$#
 
